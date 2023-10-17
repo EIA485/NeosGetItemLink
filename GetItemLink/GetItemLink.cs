341e15612a45a4b815cf30b6392b708edfbb5126
LeCloutPanda <53411604+LeCloutPanda@users.noreply.github.com>
2023-10-18 03:32:06 +1100

    Fixed up csproj
    Arti as cool as you are just say I fucked it and let me fix it :(

--- GetItemLink/GetItemLink.cs
+++ GetItemLink/GetItemLink.cs
@@ -15,7 +15,7 @@ namespace GetItemLink
         public override string Name => "GetItemLink";
         public override string Author => "eia485";
         public override string Version => "1.4.2";
-        public override string Link => "https://github.com/eia485/NeosGetItemLink/";
+        public override string Link => "https://github.com/eia485/GetItemLink/";
 
         // Ported by LeCloutPanda
 
